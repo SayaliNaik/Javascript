056b7986fe42ef9932d384de65610cb426579a72
SayaliNaik <21sayalinaik@gmail.com>
2021-04-03 11:08:01 -0500

    Writing functions now

--- basic.js
+++ basic.js
@@ -1,1 +1,40 @@
- 
+//var
+
+// var name = "Sayali";
+// var name = "Ankit";
+// alert(name); 
+
+//Const
+
+// const name = 'Ankit';
+// alert(name);
+
+//Let
+
+// if(true){
+// 	let name = 'Ankit';
+// 	name = "Sayali";
+// 	alert(name);
+// }
+
+//Template Strings
+
+// let fname = 'Sayali';
+// let lname = 'Naik';
+// let age = prompt("Guess Sayali's age");
+
+// old way
+// let result = fname + ' ' + lname + ' ' + 'is ' + age + ' years old';
+
+// let result = `${fname} ${lname} is ${age} years old`;
+
+// alert(result);
+
+function welcome(user, message){
+	alert(`Hello ${user}, ${message}`);
+}
+
+welcome();
+
+
+
